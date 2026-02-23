78157605c50d1761c06c72f442692a8b7f910809
Vivekmaurya1 <vivekmaurya8273@gmail.com>
2026-02-24 04:05:51 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://career-planner-agent-2.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
