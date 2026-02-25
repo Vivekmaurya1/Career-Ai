93e34726e234b6df6a5acf59ab225626555e037c
Vivekmaurya1 <vivekmaurya8273@gmail.com>
2026-02-25 15:04:09 +0530

    fixing build issue

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import Login      from "./pages/login";
 import Register   from "./pages/register";
 import Dashboard  from "./pages/Dashboard";
 import RoadmapPage from "./pages/RoadmapPage";
-import Settings   from "./pages/settings";
+import Settings   from "./pages/Settings";
 import { KeepAlive } from "./hooks/Usekeepalive";
 
 import { useAuth } from "./context/AuthContext";
