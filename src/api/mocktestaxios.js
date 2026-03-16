16e86323e1707b2b603bdeda50faea69c60ac6aa
Vivekmaurya1 <vivekmaurya8273@gmail.com>
2026-03-16 22:17:31 +0530

    Update mocktestaxios.js

--- src/api/mocktestaxios.js
+++ src/api/mocktestaxios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const mockAxios = axios.create({
-  baseURL: "https://career-planner-agent-mocktest.onrender.com/",
+  baseURL: "https://roadmapiq.vercel.app/",
   headers: { "Content-Type": "application/json" },
   timeout: 120000,
 });
