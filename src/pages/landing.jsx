d7b773ffce170f09b5957c752afc23c6130bfad5
Vivekmaurya1 <vivekmaurya8273@gmail.com>
2026-04-19 23:44:47 +0530

    Update landing.jsx

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,6 +1,6 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
-import Features from "../components/Feature";
+import Features from "../components/feature";
 import HowItWorks from "../components/HowItWorks";
 import { CTA } from "../components/CTA";
 import { Footer } from "../components/Footer";
